ddb6ea3fc96964395f88ca76320b1210cb256bf6
kangrachel <mango5890@naver.com>
2019-06-13 04:21:00 +0900

    [UPLOAD]

---
+++ project/ccei/main.js
@@ -0,0 +1,10 @@
+//slide
+$(document).ready(function(){
+    $('.slider').bxSlider({
+        auto: true,
+        autoControls: true,
+        stopAutoOnClick: true,
+        pager: true,
+        slideWidth: 1400,
+      });
+});
